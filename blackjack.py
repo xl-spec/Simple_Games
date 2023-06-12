776b9eef727082a3c4e3c86b1c1d821b40e9becd
XinLiGitHub <augurship@gmail.com>
2023-06-11 20:29:23 -0400

    bit of progress on the game

--- blackjack.py
+++ blackjack.py
@@ -23,12 +23,16 @@ flag = True
 # print(list_cards)
 player_hand = []
 computer_hand = []
+
+player_bust = False
+computer_bust = False
 # player_hand.append(list_cards[0])
 # list_cards.pop(0)
-player_count = 0
-computer_count = 0
+
 
 while flag:
+    player_count = 0
+    computer_count = 0
     if not player_hand and list_cards:
         # print("?")
         player_hand.append(list_cards[0])
@@ -37,25 +41,45 @@ while flag:
         # print("?")
         computer_hand.append(list_cards[0])
         list_cards.pop(0)
-    print(player_hand)
-    print(computer_hand)
-    # print(list_cards)
-    answer = input("hit or pass? (h/p)")
+    for i in player_hand:
+        player_count += dict_cards[i]
 
-    if answer.lower() == "h":
-        player_hand.append(list_cards[0])
-        list_cards.pop(0)
+    for j in computer_hand:
+        computer_count += dict_cards[j]
+
+    print(f"player_hand: {player_hand}")
+    print(f"player_count: {player_count}")
+    print(f"computer_hand: {computer_hand}")
+    print(f"computer_count: {computer_count}")
+    # print(list_cards)
+    if not player_bust:
+        answer = input("hit or pass? (h/p)")
+
+        if answer.lower() == "h":
+            player_hand.append(list_cards[0])
+            list_cards.pop(0)
     # else:
     #     pass
-    if random.choice(["h", "p"]) == "h":
-        computer_hand.append(list_cards[0])
-        list_cards.pop(0)
+    if not computer_bust:
+        if random.choice(["h", "p"]) == "h":
+            computer_hand.append(list_cards[0])
+            list_cards.pop(0)
+            print("computer chose hit")
+        else:
+            print("computer chose pass")
 
     for i in player_hand:
         player_count += dict_cards[i]
     for i in computer_hand:
         computer_count += dict_cards[i]
         # not done but outline okay
-    print(player_count)
-    print(computer_count)
+    # print(player_count)
+    # print(computer_count)
     # flag = False
+    if player_count > 21:
+        player_bust = True
+    if computer_count > 21:
+        computer_bust = True
+
+    if player_bust and computer_bust:
+        flag = False
